26a3a72e57a06aa98b2e366420bb5d237e09b898
Costin <costin@befair.it>
2017-09-25 12:29:05 +0200

    [DOC] Add new configuration files + minor fixes

---
+++ doc-dev/source/_static/set_wifi-html-connect.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<p>Reboot device to complete!</p>
+<br>
+<?php
+
+$vconf = $_POST["conf"];
+$vssid = $_POST["ssid"];
+$vpsw = $_POST["password"];
+$apice = '"';
+
+$file = fopen("/var/lib/wifi_config/wifi_config.txt", "w") or die("Unable to open file!");
+
+fwrite($file, "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n");
+fwrite($file, "update_config=1\n");
+
+fwrite($file, "network={\n");
+fwrite($file,	"ssid=$apice$vssid$apice\n");
+fwrite($file,	"psk=$apice$vpsw$apice\n");
+fwrite($file, "}\n");
+
+fclose($file);
+
+$file_invt = fopen("/var/lib/wifi_config/wifi_config_invt.txt", "w") or die("Unable to open file!");
+fwrite($file_invt, "$vconf");
+$fclose($file_invt);
+?>
+</body>
+</html>
+
---
+++ doc-dev/source/_static/set_wifi-wifi_config.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#Variables
+FILE_IN=/var/lib/wifi_config/wifi_config.txt
+FILE_OUT=/etc/wpa_supplicant/wpa_supplicant
+
+FILE_IN_INVT_CHECK=/var/lib/wifi_config/wifi_config_invt.txt
+FILE_IN_INVT_SMA=/var/www/html/nginx/123solar/config/config_sma/config_invt1.php
+FILE_IN_INVT_AURORA=/var/www/html/nginx/123solar/config/config_aurora/config_invt1.php
+FILE_OUT_INVT=/var/www/html/nginx/123solar/config/config_invt1.php
+
+#Check if file exists
+if [ -f $FILE_IN ]; then
+	#Write data to new file
+	cp $FILE_IN $FILE_OUT
+fi
+
+#Check if file exists
+if [ -f $FILE_IN_INVT_CHECK ]; then
+	if grep -Fxq "aurora" $FILE_IN_INVT_CHECK; then
+		cp $FILE_IN_INVT_AURORA $FILE_OUT_INVT
+	fi
+	if grep -Fxq "sma" $FILE_IN_INVT_CHECK; then
+		cp $FILE_IN_INVT_SMA $FILE_OUT_INVT
+        fi
+fi
