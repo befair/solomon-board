e832f388ee7e4146891711c604d0e95ed2a8f179
Costin <costin@befair.it>
2017-09-28 14:45:46 +0200

    [Fix] Minor Fix

--- doc-dev/source/_static/set_wifi-html-connect.php
+++ doc-dev/source/_static/set_wifi-html-connect.php
@@ -9,7 +9,7 @@ $vssid = $_POST["ssid"];
 $vpsw = $_POST["password"];
 $apice = '"';
 
-$file = fopen("/var/lib/wifi_config/wifi_config.txt", "w") or die("Unable to open file!");
+$file = fopen("/var/lib/wifi_config.txt", "w") or die("Unable to open file!");
 
 fwrite($file, "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n");
 fwrite($file, "update_config=1\n");
@@ -21,7 +21,7 @@ fwrite($file, "}\n");
 
 fclose($file);
 
-$file_invt = fopen("/var/lib/wifi_config/wifi_config_invt.txt", "w") or die("Unable to open file!");
+$file_invt = fopen("/var/lib/wifi_config_invt.txt", "w") or die("Unable to open file!");
 fwrite($file_invt, "$vconf");
 $fclose($file_invt);
 ?>
--- doc-dev/source/_static/set_wifi-wifi_config.sh
+++ doc-dev/source/_static/set_wifi-wifi_config.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #Variables
-FILE_IN=/var/lib/wifi_config/wifi_config.txt
+FILE_IN=/var/lib/wifi_config.txt
 FILE_OUT=/etc/wpa_supplicant/wpa_supplicant
 
-FILE_IN_INVT_CHECK=/var/lib/wifi_config/wifi_config_invt.txt
-FILE_IN_INVT_SMA=/var/www/html/nginx/123solar/config/config_sma/config_invt1.php
-FILE_IN_INVT_AURORA=/var/www/html/nginx/123solar/config/config_aurora/config_invt1.php
-FILE_OUT_INVT=/var/www/html/nginx/123solar/config/config_invt1.php
+FILE_IN_INVT_CHECK=/var/lib/wifi_config_invt.txt
+FILE_IN_INVT_SMA=/var/www/html/123solar/config/config_sma/config_invt1.php
+FILE_IN_INVT_AURORA=/var/www/html/123solar/config/config_aurora/config_invt1.php
+FILE_OUT_INVT=/var/www/html/123solar/config/config_invt1.php
 
 #Check if file exists
 if [ -f $FILE_IN ]; then
