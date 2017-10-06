cd688e1d8441c47e57d369c196fe99a46a5e8099
Costin <costin@befair.it>
2017-10-06 12:19:42 +0200

    [Review] Updated script
    Changed unlink to start_openssh-server.service

--- doc-dev/source/_static/start_openssh-server.sh
+++ doc-dev/source/_static/start_openssh-server.sh
@@ -4,4 +4,4 @@ rm /etc/ssh/ssh_host_*key*
 dpkg-reconfigure openssh-server
 systemctl restart ssh
 #The service will run only for the first time, or until reload.
-unlink /etc/systemd/system/default.target.wants/openssh-server_start.service
+unlink /etc/systemd/system/default.target.wants/start_openssh-server.service
